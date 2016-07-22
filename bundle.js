f2c2dab94fcb1329a79d2690418d072db707c776
Audrey Burk <audrey.jo.burk@gmail.com>
2016-07-22 11:03:11 -0700

    added... snow?

--- bundle.js
+++ bundle.js
@@ -176,6 +176,12 @@
 	  return hsla;
 	};
 	
+	Color.prototype.star = function () {
+	    const hsla = `hsla(${this.h}, 75%, 50%, .8)`;
+	    // return hsla;
+	    return "white";
+	};
+	
 	Color.prototype.sail = function (dif) {
 	  // stands out too much on light bg as is
 	  // alter lightness/opacity based on base lightness??
@@ -193,6 +199,7 @@
 
 	const Wave = __webpack_require__(5);
 	const Ship = __webpack_require__(8);
+	const Star = __webpack_require__(9);
 	
 	function Layer(depth, canvas){
 	  this.depth = depth;
@@ -212,14 +219,29 @@
 	    const ship = new Ship(this.wave, this.canvas.ctx, this.ratio);
 	    this.ships.push(ship);
 	  }
+	
+	  this.stars = [];
 	};
 	
 	Layer.prototype.render = function (tide) {
+	  this.addStars();
+	
 	  this.ships.forEach( ship => {
 	    ship.move();
 	    ship.render();
 	  });
+	  this.stars.forEach( star => {
+	    star.move();
+	    star.render();
+	  });
 	  this.wave.render(tide); // globalize game for easy tide reference??
+	};
+	
+	Layer.prototype.addStars = function () {
+	  if(Math.floor(Math.random() * 200) === 0){
+	    const star = new Star(this.wave, this.canvas.ctx, this.ratio);
+	    this.stars.push(star);
+	  }
 	};
 	
 	module.exports = Layer;
@@ -450,10 +472,8 @@
 	
 	
 	  if(this.x < this.wave.points[0].x){
-	    console.log(this.x);
 	    this.x = window.innerWidth + 50;
 	  } else if(this.x > this.wave.points[this.wave.points.length - 1].x){
-	    console.log(this.x);
 	    this.x = -50;
 	  }
 	
@@ -486,6 +506,71 @@
 	
 	
 	// y(t) = (y0−2y1+y2)t^2 + (2y1−2y0)t + y0
+
+
+/***/ },
+/* 9 */
+/***/ function(module, exports, __webpack_require__) {
+
+	const Color = __webpack_require__(3);
+	
+	function Star(wave, ctx, ratio){
+	  this.wave = wave;
+	  this.ctx = ctx;
+	  this.ratio = ratio;
+	
+	  this.x = Math.random() * window.innerWidth;
+	  this.y = Math.random() * 20 + 20;
+	}
+	
+	Star.prototype.move = function () {
+	  this.x += Math.random();
+	  this.y += Math.random() * 2;
+	  // for(let i = 0; i < this.wave.points.length; i++){
+	  //   const point = this.wave.points[i];
+	  //   if(point.x > this.x){
+	  //     const prevPoint = this.wave.points[i-1];
+	  //
+	  //     if(){
+	  //
+	  //     } else {
+	  //
+	  //       const total = Math.abs(point.x - prevPoint.x)
+	  //       const left = Math.abs(this.x - prevPoint.x);
+	  //       const right = Math.abs(this.x - point.x);
+	  //       const leftWeight = right / total; // opposite on purpose
+	  //       const rightWeight = left / total; // closer should mean bigger, not smaller
+	  //
+	  //       this.y = (prevPoint.y * leftWeight + point.y * rightWeight);
+	  //       const heightWidthRatio = (point.y - prevPoint.y) / (point.x - prevPoint.x);
+	  //
+	  //       // maybe make use of the tide variable when determining x movement
+	  //       this.x += 6 * this.ratio * heightWidthRatio * (leftWeight < rightWeight ? leftWeight : rightWeight);
+	  //       this.tilt = (Math.PI / 2) * heightWidthRatio * (leftWeight < rightWeight ? leftWeight : rightWeight);
+	  //     }
+	  //     break
+	  //   }
+	};
+	
+	
+	Star.prototype.render = function () {
+	  const ratio = this.ratio;
+	  this.ctx.fillStyle = Color.star();
+	
+	  this.ctx.save();
+	
+	  this.ctx.beginPath();
+	  const begin = .2 + this.tilt;
+	  const end = Math.PI-.2 + this.tilt;
+	  this.ctx.arc(this.x, this.y, 10 * ratio, 0, Math.PI * 2);
+	  this.ctx.fill();
+	  this.ctx.closePath();
+	
+	  this.ctx.restore();
+	};
+	
+	
+	module.exports = Star;
 
 
 /***/ }
