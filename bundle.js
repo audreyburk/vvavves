c8c451593b717fd5a0be91d9f88ea39ea52b5ced
jordan <jordan.g.burk@gmail.com>
2016-07-23 14:12:43 -0700

    bundle conflicts

--- bundle.js
+++ bundle.js
@@ -59,7 +59,7 @@
 	
 	// global singleton canvas, or too dangerous?
 	
-	const Listener = __webpack_require__(7);
+	const Listener = __webpack_require__(8);
 	
 	function Game(){
 	  this.canvas = new Canvas;
@@ -192,7 +192,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	const Wave = __webpack_require__(5);
-	const Ship = __webpack_require__(8);
+	const Ship = __webpack_require__(10);
 	
 	function Layer(depth, canvas){
 	  this.depth = depth;
@@ -229,7 +229,7 @@
 /* 5 */
 /***/ function(module, exports, __webpack_require__) {
 
-	const Point = __webpack_require__(6);
+	const Point = __webpack_require__(9);
 	const Color = __webpack_require__(3);
 	
 	function Wave(canvas, depth, ratio) {
@@ -306,50 +306,9 @@
 
 
 /***/ },
-/* 6 */
-/***/ function(module, exports, __webpack_require__) {
-
-	const Listener = __webpack_require__(7);
-	
-	function Point(x, y, oldY, ratio){
-	  this.x = x;
-	  this.y = y;
-	  this.oldY = oldY;
-	  this.ratio = ratio;
-	
-	  this.angle = Math.random() * 360;
-	  this.speed = 0.0075 + Math.random()*0.0275;
-	  this.amplitude = Math.random() * 10 + 30;
-	}
-	
-	Point.generatePoints = function(width, height, y, ratio, spacing){
-	  const yCenter = height / 2;
-	  const points = [];
-	
-	  for (let x = -(spacing * 2); x <= width + spacing * 2; x += spacing) {
-	    let randomOffset = Math.random() * 60 - 30;
-	    const point = new Point(
-	      x + (Math.random()*20 - 10) * ratio,
-	      yCenter + y * ratio + randomOffset * ratio + Math.random() * 20,
-	      yCenter + y * ratio + randomOffset * ratio + Math.random() * 20,
-	      ratio
-	    );
-	    points.push(point);
-	  }
-	  return points;
-	};
-	
-	Point.prototype.move = function (tide) {
-	  this.y = this.oldY + Math.sin(this.angle) * this.amplitude * Listener.mouseY * this.ratio * .5 + Math.sin(this.angle) * this.amplitude * .5;
-	  this.x += tide * this.ratio;
-	  this.angle += 1.5 * this.speed; //* Math.abs(Listener.mouseX) + .5 * this.speed;
-	};
-	
-	module.exports = Point;
-
-
-/***/ },
-/* 7 */
+/* 6 */,
+/* 7 */,
+/* 8 */
 /***/ function(module, exports) {
 
 	const _viableKeys = [37, 38, 39, 40];
@@ -391,10 +350,53 @@
 
 
 /***/ },
-/* 8 */
+/* 9 */
 /***/ function(module, exports, __webpack_require__) {
 
-	const Listener = __webpack_require__(7);
+	const Listener = __webpack_require__(8);
+	
+	function Point(x, y, oldY, ratio){
+	  this.x = x;
+	  this.y = y;
+	  this.oldY = oldY;
+	  this.ratio = ratio;
+	
+	  this.angle = Math.random() * 360;
+	  this.speed = 0.0075 + Math.random()*0.0275;
+	  this.amplitude = Math.random() * 10 + 30;
+	}
+	
+	Point.generatePoints = function(width, height, y, ratio, spacing){
+	  const yCenter = height / 2;
+	  const points = [];
+	
+	  for (let x = -(spacing * 2); x <= width + spacing * 2; x += spacing) {
+	    let randomOffset = Math.random() * 60 - 30;
+	    const point = new Point(
+	      x + (Math.random()*20 - 10) * ratio,
+	      yCenter + y * ratio + randomOffset * ratio + Math.random() * 20,
+	      yCenter + y * ratio + randomOffset * ratio + Math.random() * 20,
+	      ratio
+	    );
+	    points.push(point);
+	  }
+	  return points;
+	};
+	
+	Point.prototype.move = function (tide) {
+	  this.y = this.oldY + Math.sin(this.angle) * this.amplitude * Listener.mouseY * this.ratio * .5 + Math.sin(this.angle) * this.amplitude * .5;
+	  this.x += tide * this.ratio;
+	  this.angle += 1.5 * this.speed; //* Math.abs(Listener.mouseX) + .5 * this.speed;
+	};
+	
+	module.exports = Point;
+
+
+/***/ },
+/* 10 */
+/***/ function(module, exports, __webpack_require__) {
+
+	const Listener = __webpack_require__(8);
 	const Color = __webpack_require__(3);
 	
 	function Ship(wave, ctx, ratio){
